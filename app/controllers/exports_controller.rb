f29d845b2f4d7b847efafc62a96dc63e75bfaacc
Jesse Tilro <jesse@jessetilro.nl>
2017-03-02 20:30:02 +0100

    Update PDF rendering and interface.

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -3,11 +3,12 @@ class ExportsController < ApplicationController
   def show
     @sources = Source.all
     @users = User.all
+    disposition = params.key?(:download) ? 'attachment' : 'inline'
     respond_to do |format|
       format.pdf do
         render  pdf: 'export',
                 template: 'exports/export',
-                disposition: 'attachment'
+                disposition: disposition
       end
     end
   end
