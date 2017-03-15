ba872c9a8cb4cf6bbfc1348a9c395ea2651e761a
Jesse Tilro <jesse@jessetilro.nl>
2017-03-15 12:03:14 +0100

    Fix javascripts document ready to turbolinks:load.

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).on('turbolinks:load', function(){
   var typingTimer;
   var typingInterval = 500;
 
--- app/assets/javascripts/sources.js
+++ app/assets/javascripts/sources.js
@@ -1,3 +1,3 @@
-$(function () {
+$(document).on('turbolinks:load', function () {
   $('[data-toggle="popover"]').popover()
-})
+});
