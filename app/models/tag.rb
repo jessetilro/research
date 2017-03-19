59c46a6b83e7580a3970336fbe88ece9e3018f70
Jesse Tilro <jesse@jessetilro.nl>
2017-03-19 16:48:56 +0100

    Add Tag color validation.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,5 +3,10 @@ class Tag < ApplicationRecord
   has_and_belongs_to_many :sources, join_table: :taggings
 
   validates :name, presence: true, uniqueness: true
+  validates :color, presence: false, format: { with: /\A(?:#(?:[a-f0-9]{3}|[a-f0-9]{6})|)\z/, message: 'must be a valid hexadecimal color representation.' }
+
+  def color= c
+    super c.downcase
+  end
 
 end
