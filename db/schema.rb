12ef3588b501d2b1bd888270801c178e83d93bf8
Jesse Tilro <jesse@jessetilro.nl>
2017-03-19 16:26:53 +0100

    Add color to Tag.

--- db/schema.rb
+++ db/schema.rb
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20170319105037) do
   create_table "tags", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
+    t.string   "color"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
