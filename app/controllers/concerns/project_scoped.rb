6a3d3c1d96a2dc78598c0dab4dcaea345e46b75b
Jesse Tilro <jesse@jessetilro.nl>
2018-10-03 14:34:59 +0200

    Update projects lists in order of most recently used.

--- app/controllers/concerns/project_scoped.rb
+++ app/controllers/concerns/project_scoped.rb
@@ -6,13 +6,13 @@ module ProjectScoped
   end
 
   protected
+
   def load_project
-    @projects = current_user.projects
+    @projects = current_user.projects.ordered_by_last_usage
     @project = params[:project_id].present? ? @projects.find(params[:project_id]) : @projects.first
     # TODO make this more efficient
     @participation = @project.participations.find_by(user: current_user)
     @participation.touch :last_used_at
     authorize! :read, ([@project] + @projects)
   end
-
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,7 @@ class Project < ApplicationRecord
   has_many :participations
   has_many :users, through: :participations
 
+  scope :ordered_by_last_usage, -> {
+    order('participations.last_used_at DESC')
+  }
 end
