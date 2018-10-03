1d38d0c731ddc7facfbc9982dfd4cc950843b867
Jesse Tilro <jesse@jessetilro.nl>
2018-10-03 14:34:23 +0200

    Add logging of last project usage.

--- app/controllers/concerns/project_scoped.rb
+++ app/controllers/concerns/project_scoped.rb
@@ -9,6 +9,9 @@ module ProjectScoped
   def load_project
     @projects = current_user.projects
     @project = params[:project_id].present? ? @projects.find(params[:project_id]) : @projects.first
+    # TODO make this more efficient
+    @participation = @project.participations.find_by(user: current_user)
+    @participation.touch :last_used_at
     authorize! :read, ([@project] + @projects)
   end
 
