b7efa1e82a41493ebfbc9a0145c96912ba991759
Jesse Tilro <jesse@jessetilro.nl>
2019-02-20 17:03:44 +0100

    Temporarily use paperclip for migration.

--- app/models/source.rb
+++ app/models/source.rb
@@ -45,22 +45,14 @@ class Source < ApplicationRecord
 
   default_scope { sorted_by_time }
 
-  # has_attached_file :document
-  # validates_attachment_content_type :document, content_type: /pdf/
+  has_attached_file :document
+  validates_attachment_content_type :document, content_type: /pdf/
 
-  has_one_attached :document
-  validates :document, file_content_type: {
-    allow: ['application/pdf'],
-    if: -> { document.attached? }
-  }
-
-
-  # -  validates_attachment_content_type :avatar, content_type: /\Aimage/
-  # -  validates_attachment_file_name :avatar, matches: /jpe?g\z/
-  # +  validates :avatar, file_content_type: {
-  # +    allow: ["image/jpeg", "image/png"],
-  # +    if: -> { avatar.attached? },
-  # +  }
+  # has_one_attached :document
+  # validates :document, file_content_type: {
+  #   allow: ['application/pdf'],
+  #   if: -> { document.attached? }
+  # }
 
   def shortest_title; short_title || title; end
 
