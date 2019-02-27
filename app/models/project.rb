d1030699dbd72443eda8f6292aa03a4f905410f0
Jesse Tilro <jesse@jessetilro.nl>
2019-02-27 14:34:51 +0100

    Fix ordering projects by last used nulls last in postgresql.

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,7 @@ class Project < ApplicationRecord
   has_many :users, through: :participations
 
   scope :ordered_by_last_usage, -> {
-    order('participations.last_used_at DESC')
+    order('participations.last_used_at DESC NULLS LAST')
   }
 
   def self.most_recently_used
