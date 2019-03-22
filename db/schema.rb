dab2a40e3b7d13e32150da4e8094e309e765b2ac
Jesse Tilro <jesse@jessetilro.nl>
2019-03-22 14:59:58 +0100

    Update user to be confirmable.

--- db/schema.rb
+++ db/schema.rb
@@ -10,143 +10,149 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_160834) do
+ActiveRecord::Schema.define(version: 2019_03_22_131647) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "fuzzystrmatch"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.string "name", limit: 255, null: false
+    t.string "record_type", limit: 255, null: false
+    t.decimal "record_id", null: false
+    t.decimal "blob_id", null: false
     t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+    t.index ["blob_id"], name: "idx_37159_index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "idx_37159_index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
+    t.string "key", limit: 255, null: false
+    t.string "filename", limit: 255, null: false
+    t.string "content_type", limit: 255
     t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.decimal "byte_size", null: false
+    t.string "checksum", limit: 255, null: false
     t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+    t.index ["key"], name: "idx_37168_index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "participations", force: :cascade do |t|
-    t.integer "project_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "project_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "last_used_at"
-    t.datetime "created_at", default: "2017-02-27 20:54:00", null: false
-    t.datetime "updated_at", default: "2017-02-27 20:54:00", null: false
-    t.index ["project_id"], name: "index_participations_on_project_id"
-    t.index ["user_id"], name: "index_participations_on_user_id"
+    t.datetime "created_at", default: "2017-02-27 19:54:00", null: false
+    t.datetime "updated_at", default: "2017-02-27 19:54:00", null: false
+    t.index ["project_id"], name: "idx_37183_index_participations_on_project_id"
+    t.index ["user_id"], name: "idx_37183_index_participations_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
+    t.string "name", limit: 255
+    t.string "description", limit: 255
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "rating"
+    t.bigint "rating"
     t.text "comment"
-    t.integer "user_id"
-    t.integer "source_id"
+    t.bigint "user_id"
+    t.bigint "source_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["source_id"], name: "index_reviews_on_source_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
+    t.index ["source_id"], name: "idx_37200_index_reviews_on_source_id"
+    t.index ["user_id"], name: "idx_37200_index_reviews_on_user_id"
   end
 
   create_table "sources", force: :cascade do |t|
-    t.string "title"
+    t.string "title", limit: 255
     t.text "authors"
-    t.integer "year"
+    t.bigint "year"
     t.text "url"
     t.text "abstract"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "search_query"
-    t.string "search_database"
+    t.string "search_database", limit: 255
     t.text "keywords"
-    t.integer "stars_count", default: 0
-    t.integer "bibtex_type", default: 0
-    t.string "bibtex_key"
-    t.string "isbn"
-    t.string "doi"
+    t.bigint "stars_count", default: 0
+    t.bigint "bibtex_type", default: 0
+    t.string "bibtex_key", limit: 255
+    t.string "isbn", limit: 255
+    t.string "doi", limit: 255
     t.text "editors"
-    t.string "subtitle"
-    t.string "shorttitle"
-    t.integer "month"
-    t.string "publisher"
-    t.string "institution"
-    t.string "organization"
+    t.string "subtitle", limit: 255
+    t.string "shorttitle", limit: 255
+    t.bigint "month"
+    t.string "publisher", limit: 255
+    t.string "institution", limit: 255
+    t.string "organization", limit: 255
     t.text "address"
-    t.string "school"
-    t.string "edition"
-    t.string "series"
-    t.integer "chapter"
-    t.string "pages"
-    t.string "journal"
-    t.integer "number"
-    t.integer "volume"
+    t.string "school", limit: 255
+    t.string "edition", limit: 255
+    t.string "series", limit: 255
+    t.bigint "chapter"
+    t.string "pages", limit: 255
+    t.string "journal", limit: 255
+    t.bigint "number"
+    t.bigint "volume"
     t.text "note"
-    t.integer "project_id"
-    t.index ["project_id"], name: "index_sources_on_project_id"
-    t.index ["user_id"], name: "index_sources_on_user_id"
+    t.bigint "project_id"
+    t.index ["project_id"], name: "idx_37212_index_sources_on_project_id"
+    t.index ["user_id"], name: "idx_37212_index_sources_on_user_id"
   end
 
   create_table "stars", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "source_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "source_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["source_id"], name: "index_stars_on_source_id"
-    t.index ["user_id"], name: "index_stars_on_user_id"
+    t.index ["source_id"], name: "idx_37223_index_stars_on_source_id"
+    t.index ["user_id"], name: "idx_37223_index_stars_on_user_id"
   end
 
   create_table "taggings", id: false, force: :cascade do |t|
-    t.integer "source_id", null: false
-    t.integer "tag_id", null: false
-    t.index ["source_id"], name: "index_taggings_on_source_id"
-    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.bigint "source_id", null: false
+    t.bigint "tag_id", null: false
+    t.index ["source_id"], name: "idx_37227_index_taggings_on_source_id"
+    t.index ["tag_id"], name: "idx_37227_index_taggings_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 255
     t.text "description"
-    t.string "color"
+    t.string "color", limit: 255
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "project_id"
-    t.index ["project_id"], name: "index_tags_on_project_id"
+    t.bigint "project_id"
+    t.index ["project_id"], name: "idx_37232_index_tags_on_project_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
+    t.string "email", limit: 255, default: "", null: false
+    t.string "encrypted_password", limit: 255, default: "", null: false
+    t.string "reset_password_token", limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
+    t.bigint "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
+    t.string "current_sign_in_ip", limit: 255
+    t.string "last_sign_in_ip", limit: 255
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.integer "role", default: 0
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.string "first_name", limit: 255
+    t.string "last_name", limit: 255
+    t.bigint "role", default: 0
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "idx_37241_index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "idx_37241_index_users_on_reset_password_token", unique: true
   end
 
 end
