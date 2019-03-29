23ffd90419dbfccea3a8f0c2244da3fdd6572481
Jesse Tilro <jesse@jessetilro.nl>
2019-03-29 19:39:57 +0100

    Fix redirect to projects page when no project available yet.

--- app/controllers/concerns/project_scoped.rb
+++ app/controllers/concerns/project_scoped.rb
@@ -10,6 +10,7 @@ module ProjectScoped
   def load_project
     @projects = current_user.projects.ordered_by_last_usage
     @project = params[:project_id].present? ? @projects.find(params[:project_id]) : @projects.first
+    return redirect_to projects_url if @project.blank?
     # TODO make this more efficient
     @participation = @project.participations.find_by(user: current_user)
     @participation.touch :last_used_at
