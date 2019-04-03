ed25e5c5a25879d153fe02cf7bec50ee0f3062fd
Jesse Tilro <jesse@jessetilro.nl>
2019-04-03 14:15:37 +0200

    Fix tag name uniqueness validation scoped to project.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,7 +4,7 @@ class Tag < ApplicationRecord
 
   has_and_belongs_to_many :sources, join_table: :taggings
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { scope: :project_id }
   validates :color, presence: false, format: { with: /\A(?:#(?:[a-f0-9]{3}|[a-f0-9]{6})|)\z/, message: 'must be a valid hexadecimal color representation.' }
 
   def color= c
@@ -14,5 +14,4 @@ class Tag < ApplicationRecord
   def effective_color
     color.present? ? color : '#333'
   end
-
 end
